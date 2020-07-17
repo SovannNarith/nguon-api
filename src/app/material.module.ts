b9fe44d2c0f2f398358ede4729a86286f40f136b
Sovann Narith <narithsovann6@gmail.com>
2020-07-17 16:13:26 +0700

    materail success

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+        MatToolbarModule,
+        MatButtonModule
+    ],
+    exports: [
+        MatToolbarModule,
+        MatButtonModule
+    ]
 })
 
 export class MaterialModule{}
