cce2da87274076952b2f9b2cf63ade1cbd4c7959
Sovann Narith <narithsovann6@gmail.com>
2020-07-21 01:54:33 +0700

    authorize

--- src/app/authen/login/login.component.ts
+++ src/app/authen/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenService } from 'src/app/services/authen.service';
-import { FormGroup, FormControl } from '@angular/forms';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,22 +11,25 @@ export class LoginComponent implements OnInit {
   fmGroup: FormGroup;
   constructor(private authen: AuthenService) {
     this.fmGroup = new FormGroup({
-      'email': new FormControl(),
-      'password': new FormControl()
+      'email': new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      'password': new FormControl('', [Validators.required])
     });
    }
 
   ngOnInit(): void {
   }
 
-  canActivate(){
+  getToken(){
     return localStorage.getItem('data')? true : false;
   }
-
   onSubmit(email: string, password: string){
-    this.authen.login(email.toString(),password.toString()).subscribe( data => {
+    this.authen.login(email.toString(),password.toString())
+    .subscribe( data => {
       localStorage.setItem('data',JSON.stringify(data));
-      console.log(data);
+      console.log(localStorage.getItem('data'));
     });
   }
   
@@ -35,7 +37,7 @@ export class LoginComponent implements OnInit {
     this.authen.logout().subscribe( data => {
       console.log(data);
     });
-    localStorage.removeItem('data');
+    localStorage.clear();
   }
 
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,19 +3,21 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input";
-
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 @NgModule({
     imports: [
         MatToolbarModule,
         MatButtonModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatProgressSpinnerModule
     ],
     exports: [
         MatToolbarModule,
         MatButtonModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatProgressSpinnerModule
     ]
 })
 
